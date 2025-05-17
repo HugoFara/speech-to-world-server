8d2bc3309dbf02d43e5bd5c7ae8888dc2e5cb508
HugoFara <hugo.farajallah@protonmail.com>
2025-05-17 17:01:47 +0200

    fix(server): configuration file could not be found.

--- src/speech_to_world_hugofara/server/utils.py
+++ src/speech_to_world_hugofara/server/utils.py
@@ -51,7 +51,10 @@ def get_configuration_data():
     if os.path.exists("api.json"):
         config_file_path = "api.json"
     else:
-        config_file_path = os.path.join(os.path.dirname(__file__), "../api.json")
+        config_file_path = os.path.join(
+            os.path.dirname(__file__),
+            "../../../api.json"
+        )
     with open(config_file_path, encoding="utf-8") as file:
         configuration_data = json.load(file)
     return configuration_data
